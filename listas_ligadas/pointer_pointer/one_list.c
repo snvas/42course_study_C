9e06c38d85dc5dac888c2a4e2dc705b2149cc5a1
Soraia Novaes <sonovaes511@gmail.com>
2021-11-15 02:51:57 -0300

    lists ligada com struct

--- listas_ligadas/pointer_pointer/one_list.c
+++ listas_ligadas/pointer_pointer/one_list.c
@@ -6,7 +6,7 @@
 /*   By: snovaes <snovaes@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 12:47:27 by snovaes           #+#    #+#             */
-/*   Updated: 2021/11/15 02:51:14 by snovaes          ###   ########.fr       */
+/*   Updated: 2021/11/15 00:50:39 by snovaes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,16 @@ typedef struct node
 	struct node *next;
 } Node;
 
-typedef struct 
-{
-	Node *begin;
-	int size;
-}List;
-
-void create_list(List *list){
-	list->begin = NULL;
-	list->size = 0;
-}
-
 //procedure to insert in the beginning of a list
-void insert_in_begin(List *list, int num)
+void insert_in_begin(Node **list, int num)
 {
 	Node *new = malloc(sizeof(Node));
 
 	if (new)
 	{
 		new->value = num;
-		new->next = list->begin;
-		list->begin = new;
-		list->size++;
+		new->next = *list;
+		*list = new;
 	}
 	else
 		printf("Erro ao alocar memória!\n");
@@ -48,7 +36,7 @@ void insert_in_begin(List *list, int num)
 }
 
 //procedure to insert at the end of a list
-void insert_in_end(List *list, int num)
+void insert_in_end(Node **list, int num)
 {
 	Node *aux, *new = malloc(sizeof(Node));
 
@@ -58,22 +46,21 @@ void insert_in_end(List *list, int num)
 		new->next = NULL;
 
 		//is the first?
-		if (list->begin == NULL)
-			list->begin = new;
+		if (*list == NULL)
+			*list = new;
 		else{
-			aux = list->begin;
+			aux = *list;
 			while (aux->next)
 				aux = aux->next;
 			aux->next = new;
 		}
-		list->size++;
 	}
 	else
 		printf("Erro ao alocar memória\n");
 }
 
 //procedure to insert in the middle of a list
-void insert_in_middle(List *list, int num, int prev)
+void insert_in_middle(Node **list, int num, int prev)
 {
 	Node *aux, *new = malloc(sizeof(Node));
 
@@ -81,86 +68,94 @@ void insert_in_middle(List *list, int num, int prev)
 	{
 		new->value = num;
 		//is the first?
-		if (list->begin == NULL)
+		if (*list == NULL)
 		{
 			new->next = NULL;
-			list->begin = new;
+			*list = new;
 		}
 		else
 		{
-			aux = list->begin;
+			aux = *list;
 			while (aux->value != prev && aux->next)
 				aux = aux->next;
 			new->next = aux->next;
 			aux->next = new;
 		}
-		list->size++;
 	}
 	else
 		printf("Erro ao alocar memória\n");
 }
 
-void insert_ordered(List *list, int num)
+void insert_ordered(Node **list, int num)
 {
-	Node *aux, *new =  malloc(sizeof(Node));
+	Node *aux, *new = malloc(sizeof(Node));
 
 	if (new)
 	{
 		new->value = num;
-		if(list->begin == NULL)
+		if (*list == NULL)
 		{
 			new->next = NULL;
-			list->begin = new;
+			*list = new;
 		}
-		else if(new->value < list->begin->value)
+		else if (new->value < (*list)->value)
 		{
-			new->next = list->begin;
-			list->begin = new;
+			new->next = *list;
+			*list = new;
 		}
-		else{
-			aux = list->begin;
+		else
+		{
+			aux = *list;
 			while(aux->next && new->value > aux->next->value)
 				aux = aux->next;
 			new->next = aux->next;
 			aux->next = new;
 		}
-		list->size++;
 	}
 	else
-			printf("Erro ao alocar memória!\n");
+		printf("Erro ao alocar memória!\n");
 }
 
-Node* to_remove(List *list, int num)
+Node* to_remove(Node **list, int num)
 {
 	Node *aux, *remove = NULL;
 
-	if(list->begin)
+	if(*list)
 	{
-		if(list->begin->value == num)
+		if((*list)->value == num)
 		{
-			remove = list->begin;
-			list->begin = remove->next;
-			list->size--;
+			remove = *list;
+			*list = remove->next;
 		}
 		else{
-			aux = list->begin;
+			aux = *list;
 			while (aux->next && aux->next->value != num)
 				aux = aux->next;
 			if(aux->next)
 			{
 				remove = aux->next;
 				aux->next = remove->next;
-				list->size--;
 			}		
 		}
 	}
 	return remove;
 }
 
-void printList(List list)
+Node* search(Node **list, int num)
 {
-	Node *node = list.begin;
-	printf("\n\tLista size %d: ", list.size);
+	Node *aux, *node = NULL;
+
+	aux = *list;
+	while (aux && aux->value != num)
+		aux = aux->next;
+	if (aux)
+		node = aux;
+	return (node);
+}
+
+void printList(Node *node)
+{
+	printf("\n\tLista: ");
 	while (node)
 	{
 		printf("%d ", node->value);
@@ -172,55 +167,63 @@ void printList(List list)
 int main()
 {
 	int option, previous, value;
-	Node *removed;
-	List list;
-
-	create_list(&list);
+	Node *removed, *list = NULL;
 	
 	do{
-		printf("\n\t0 - Sair\n\t1 - InserirInicio\n\t2 - inserirFim\n\t3 - InserirMeio\n\t4 - InserirOrdenado\n\t5 - Remover\n\t6 - Imprimir\n\t");
+		printf("\n\t0 - Sair\n\t1 - InserirInicio\n\t2 - inserirFim\n\t3 - InserirMeio\n\t4 - InserirOrdenado\n\t5 - Remover\n\t6 - Imprimir\n\t7 - Pesquisar\n\t");
 		scanf("%d", &option);
+
 		switch (option){
-			case 1:
-				printf("Digite um valor: ");
-				scanf("%d", &value);
-				insert_in_begin(&list, value);
-				break;
-			case 2:
-				printf("Digite um valor: ");
-				scanf("%d", &value);
-				insert_in_end(&list, value);
-				break;
-			case 3:
-				printf("Digite um valor e a referência: ");
-				scanf("%d%d", &value, &previous);
-				insert_in_middle(&list, value, previous);
-				break;
-			case 4:
-				printf("Digite um valor: ");
-				scanf("%d", &value);
-				insert_ordered(&list, value);
-				break;
-			case 5:
-				printf("Digite um valor: ");
-				scanf("%d", &value);
-				removed = to_remove(&list, value);
-				if(removed)
-				{
-					printf("Elemento a ser removido: %d\n", removed->value);
-					free(removed);
-				}
-				else
-					printf("Número inexistente na lista!\n");
-					break;
-			case 6:
-				printList(list);
-				break;
-			default:
-				if (option != 0)
-					printf("Opcao Inválida!\n");
-			}	
-		}while (option != 0);
+		case 1:
+			printf("Digite um valor: ");
+			scanf("%d", &value);
+			insert_in_begin(&list, value);
+			break;
+		case 2:
+			printf("Digite um valor: ");
+			scanf("%d", &value);
+			insert_in_end(&list, value);
+			break;
+		case 3:
+			printf("Digite um valor e a referência: ");
+			scanf("%d%d", &value, &previous);
+			insert_in_middle(&list, value, previous);
+			break;
+		case 4:
+			printf("Digite um valor: ");
+			scanf("%d", &value);
+			insert_ordered(&list, value);
+			break;
+		case 5:
+			printf("Digite um valor a ser removido: ");
+			scanf("%d", &value);
+			removed = to_remove(&list, value);
+			if(removed)
+			{
+				printf("Elemento a ser removido: %d\n", removed->value);
+				free(removed);
+			}
+			else
+				printf("Número inexistente na lista!\n");
+			break;
+		case 6:
+			printList(list);
+			break;
+		case 7:
+			printf("Digite um valor a ser pesquisado: ");
+			scanf("%d", &value);
+			removed = search(&list, value);
+			if (removed)
+				printf("Elemento encontrado: %d\n", removed->value);
+			else
+				printf("Não encontrado!");
+			break;
+		default:
+			if(option != 0)
+				printf("Opção inválida!\n");
+		}
+	}
+	while (option != 0);
 	
-		return 0;
+	return 0;
 }
